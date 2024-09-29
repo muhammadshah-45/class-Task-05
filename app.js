4304fb3262d5a3a5f7674a11be7de54b6b6f97d6
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-09-29 10:53:47 +0500

    i add script.js file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ calculateBtn.addEventListener("click",()=>{
 
   var inputDate = new Date(document.getElementById("dataInput").value)
   var todayDate = new Date();
+  console.log(inputDate)
   // console.log(inputDate.getDate())
   // console.log(todayDate)
   
@@ -15,10 +16,29 @@ calculateBtn.addEventListener("click",()=>{
   }
   
   
+  
+  // Years 
+  var years = todayDate.getFullYear() - inputDate.getFullYear();
+  console.log(years)
+  
+  //Months 
+  var months = todayDate.getMonth() - inputDate.getMonth();
+  console.log("months",months)
+  
+  // Days
+  var days = todayDate.getDate() - inputDate.getDate();
+  console.log(days)
+  
+  
+  if(days < 0 ){
+    months--;
+    days = days + new Date(todayDate.getFullYear(), todayDate.getMonth()).getDate();
+  }
+  if (months < 0) {  
+    years--;  
+    months += 12;  
+  }  
     document.getElementById("years").innerText = years;
     document.getElementById("months").innerText = months;
     document.getElementById("days").innerText = days;
-  
-
-
 })
