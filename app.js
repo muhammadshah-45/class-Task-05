1204ce5d4419b1cdd1e103a3c381e7a546813d46
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-09-29 10:22:57 +0500

    i render the date by DOM

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ calculateBtn.addEventListener("click",()=>{
   console.log("button");
 
   var inputDate = new Date(document.getElementById("dataInput").value)
-  var todayDate = new Date()
+  var todayDate = new Date();
   console.log(inputDate)
   console.log(todayDate)
-
-  if(inputDate > todayDate){
+  if(inputDate === NaN){
+    alert("Please select the date!");
+    return
+  }
+  if(inputDate >  todayDate){
     alert("Please Select your birth date");
+
     return;
   }
   
   // Years 
   var years = todayDate.getFullYear() - inputDate.getFullYear();
-  console.log(years)
+  // console.log(years)
 
   //Months 
   var months = todayDate.getMonth() - inputDate.getMonth();
@@ -26,9 +30,17 @@ calculateBtn.addEventListener("click",()=>{
 
   if(days < 0 ){
     months--;
-    days = days + new Date(todayDate.getFullYear(), todayDate.getMonth(),0).getDate()
+    days = days + new Date(todayDate.getFullYear(), todayDate.getMonth(),0).getDate();
   }
-  
+  if (months < 0) {  
+    years--;  
+    months += 12;  
+}  
+
+
 
   document.getElementById("years").innerText = years;
+  document.getElementById("months").innerText = months;
+  document.getElementById("days").innerText = days;
+
 })
